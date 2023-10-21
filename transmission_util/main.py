9a2b609272115a556f83d075a972ea684ebdd12e
martin.almlof <martin.almlof@sebx.se>
2023-10-21 22:13:02 +0200

    Add log prints

--- transmission_util/main.py
+++ transmission_util/main.py
@@ -10,12 +10,15 @@ def main():
     now = datetime.now().astimezone(timezone.utc)
     delta = timedelta(days=30)
     cutoff = now - delta
-    torrents = c.get_torrents(None, ["doneDate","id","downloadDir"])
+    torrents = c.get_torrents(None, ["doneDate","id","name","downloadDir"])
+    print(f"A total of {len(torrents)} torrents being tracked")
     ssd_torrents = [t for t in torrents if t.download_dir.startswith(ssd_path)]
+    print(f"{len(ssd_torrents)} torrents on the SSD")
     old_ssd_torrents =  [t for t in ssd_torrents if t.done_date and t.done_date > cutoff]
+    print(f"{len(old_ssd_torrents)} old torrents on the SSD")
     for torrent in old_ssd_torrents:
         new_path = torrent.download_dir.replace(ssd_path, hdd_path)
-        print(torrent.download_dir, new_path)
+        print(f"Moving torrent #{torrent.id} '{torrent.name}' from '{torrent.download_dir}' to '{new_path}'")
         c.move_torrent_data(torrent.id, new_path)
 
 
