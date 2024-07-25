58fe07ba69dd3f26c0e143f77e48f26069fbda56
martin.almlof <martin.almlof@sebx.se>
2024-07-25 19:56:30 +0200

    Fixed predicate for old torrents. Longer timeout when moving torrents. Use Seagate3 instead of Seagate2

--- transmission_util/move_old_torrents.py
+++ transmission_util/move_old_torrents.py
@@ -3,7 +3,7 @@ from datetime import datetime,timezone,timedelta
 from transmission_rpc import Client
 
 ssd_path = "/Users/martina/media/"
-hdd_path = "/Volumes/Seagate2/"
+hdd_path = "/Volumes/Seagate3/"
 
 def main():
     c = Client(host="192.168.1.12", port=9091)
@@ -14,13 +14,14 @@ def main():
     print(f"A total of {len(torrents)} torrents being tracked")
     ssd_torrents = [t for t in torrents if t.download_dir.startswith(ssd_path)]
     print(f"{len(ssd_torrents)} torrents on the SSD")
-    old_ssd_torrents = [t for t in ssd_torrents if t.done_date and t.done_date > cutoff]
+    old_ssd_torrents = [t for t in ssd_torrents if t.done_date and t.done_date < cutoff]
     print(f"{len(old_ssd_torrents)} old torrents on the SSD")
     for torrent in old_ssd_torrents:
         new_path = torrent.download_dir.replace(ssd_path, hdd_path)
-        print(f"Moving torrent #{torrent.id} '{torrent.name}' from '{torrent.download_dir}' to '{new_path}'")
-        c.move_torrent_data(torrent.id, new_path)
+        print(f"Moving torrent #{torrent.id} '{torrent.name}' from '{torrent.download_dir}' to '{new_path}'", flush=True)
+        c.move_torrent_data(torrent.id, new_path, timeout = (5, 600))
 
 
 if __name__ == "__main__":
+    print(f"############### Starting {datetime.now()} ############")
     main()
